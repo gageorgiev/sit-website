cab87f1270c2cd9639b2494baaa421f88342e11b
unknown <gabrielgeorgiev1998@gmail.com>
2020-06-29 13:13:30 +0300

    force amplify to rebuild

--- src/components/header.js
+++ src/components/header.js
@@ -66,7 +66,7 @@ const Header = ({ className, ...props }) => {
           <HeaderLink to="/">За нас</HeaderLink>
           <HeaderLink to="/team">Проекти</HeaderLink>
           <HeaderLink to="/testimonials">Сертификати</HeaderLink>
-          <HeaderLink to="/testimonials-new">Сертификати2</HeaderLink>
+          <HeaderLink to="/testimonials-new">Сертификати(2)</HeaderLink>
           <HeaderLink to="/contact">Контакти</HeaderLink>
         </div>
       </div>
