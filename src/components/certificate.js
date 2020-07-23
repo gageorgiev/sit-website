a1d231d6c0ac989f703d84e732599a9663e7f70e
gabrielgeorgiev1998 <gabrielgeorgiev11@gmail.com>
2020-07-23 13:17:33 +0300

    animations on contacts and certs

--- src/components/certificate.js
+++ src/components/certificate.js
@@ -6,10 +6,10 @@ export default function Certificate(certdata) {
 
     return (
         <Spring
-            to={{ transform: `scale(${hovered ? 1.1 : 1})`, padding: `${hovered ? '2rem' : '0rem'}`}}
+            to={{ transform: `scale(${hovered ? 1.1 : 1})`, padding: `${hovered ? '2rem' : '0rem'}`, opacity: `${hovered ? 1 : 0.8}`}}
         >
             {props =>
-                <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} style={{ transform: props.transform, paddingLeft: props.padding, paddingRight: props.padding}}>
+                <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} style={{ transform: props.transform, paddingLeft: props.padding, paddingRight: props.padding, opacity: props.opacity}}>
                 <a href={certdata.cert} target="_blank"><img src={certdata.certpic} className="h-56 px-4 py-4"/></a>
                 </div>
             }
