5c6f0624189af98d360d87659faafd5941e506a2
gabrielgeorgiev1998 <gabrielgeorgiev11@gmail.com>
2020-07-31 17:24:14 +0300

    slider pic sizing

--- src/components/image-slider.js
+++ src/components/image-slider.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import BackgroundSlider from 'react-background-slider';
 import sliderimage1 from '../images/slider-images/dsc_0161.jpg';
-import sliderimage2 from '../images/slider-images/dsc_0018.jpg';
-import sliderimage3 from '../images/slider-images/dsc_1439.jpg';
+import sliderimage2 from '../images/slider-images/dsc_1468.jpg';
+import sliderimage3 from '../images/slider-images/dsc_1438.jpg';
 
 const ImageSlider = ({ className, ...props }) => {
     return (
         <BackgroundSlider
         className="bg-fixed"
-            images={[sliderimage1, sliderimage2, sliderimage3]}
+            images={[sliderimage1, sliderimage3, sliderimage2]}
             duration={5}
             transition={2}
         />
